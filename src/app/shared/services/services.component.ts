0c436afb050cb5f941a8a1583fdef4017d153f11
ZMALIM <lionelsh.salazar@gmail.com>
2022-01-26 20:22:54 -0500

    update portfolio

--- src/app/shared/services/services.component.ts
+++ src/app/shared/services/services.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-services',
-  templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+    selector: 'app-services',
+    templateUrl: './services.component.html',
+    styleUrls: ['./services.component.scss']
 })
 
 /**
@@ -11,36 +11,34 @@ import { Component, OnInit } from '@angular/core';
  */
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-   servicesData = [
-    {
-      icon: 'ti-settings',
-      title: 'Digital Design'
-    },
-    {
-      icon: 'ti-palette',
-      title: 'Unlimited Colors'
-    },
-    {
-      icon: 'ti-stats-up',
-      title: 'Strategy Solutions'
-    },
-    {
-      icon: 'ti-package',
-      title: 'Awesome Support'
-    },
-    {
-      icon: 'ti-dashboard',
-      title: 'Truly Multipurpose'
-    },
-    {
-      icon: 'ti-headphone',
-      title: 'Easy to customize'
-    } 
-  ]
+    servicesData = [
+        {
+            icon: 'icofont-paint',
+            title: 'Diseño web',
+            description: `Conocer las tendencias y estándares
+            relacionados con los sitios web actuales tomando
+            en cuenta los estándares de HTML5, CSS3 y Sitios Web.`
+        },
+        {
+            icon: 'icofont-code',
+            title: 'Desarrollo web',
+            description: `Desarrollo de aplicaciones a medida,
+            aplicando las últimas tendencias y estándares relacionadas
+            con la tecnología web y diseño.`
+        },
+        {
+            icon: 'icofont-automation',
+            title: 'Mantenimiento web',
+            description: `Asegurar y
+            gestionar el correcto funcionamiento de los sitios web
+            en dispositivos móviles, tablets y pantallas computadoras
+            personales.`
+        },
+    ]
 
 }
