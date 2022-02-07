5916fd2f5a256059fb07d2ce0012ef63be75115a
ZMALIM <lionelsh.salazar@gmail.com>
2022-02-07 17:08:05 -0500

    add icons and change main menu style

--- src/app/shared/skills/skills.component.ts
+++ src/app/shared/skills/skills.component.ts
@@ -23,27 +23,33 @@ export class SkillsComponent implements OnInit {
             skills: [
                 {
                     name: 'HTML5',
-                    level: '90%'
+                    level: '90%',
+                    icon: 'icon-html5',
                 },
                 {
                     name: 'CSS3',
-                    level: '75%'
+                    level: '75%',
+                    icon: 'icon-css3',
                 },
                 {
                     name: 'Bootstrap',
-                    level: '70%'
+                    level: '70%',
+                    icon: 'icon-bootstrap',
                 },
                 {
                     name: 'JavaScript',
-                    level: '85%'
+                    level: '85%',
+                    icon: 'icon-js',
                 },
                 {
                     name: 'Angular',
-                    level: '85%'
+                    level: '85%',
+                    icon: 'icon-angular',
                 },
                 {
                     name: 'React',
-                    level: '40%'
+                    level: '40%',
+                    icon: 'icon-react',
                 },
             ]
         },
@@ -53,27 +59,33 @@ export class SkillsComponent implements OnInit {
             skills: [
                 {
                     name: 'PHP',
-                    level: '85%'
+                    level: '85%',
+                    icon: 'icon-php',
                 },
                 {
                     name: 'TypeScript',
-                    level: '80%'
+                    level: '80%',
+                    icon: 'icon-js',
                 },
                 {
                     name: 'Node.js',
-                    level: '70%'
+                    level: '70%',
+                    icon: 'icon-nodejs',
                 },
                 {
                     name: 'NestJS',
-                    level: '70%'
+                    level: '70%',
+                    icon: 'icon-opensource',
                 },
                 {
                     name: 'MySQL',
-                    level: '70%'
+                    level: '70%',
+                    icon: 'icon-mysql-alt',
                 },
                 {
                     name: 'MongoDB',
-                    level: '70%'
+                    level: '70%',
+                    icon: 'icon-mongodb',
                 },
             ]
         },
@@ -84,26 +96,32 @@ export class SkillsComponent implements OnInit {
                 {
                     name: 'Git',
                     level: '85%',
+                    icon: 'icon-git',
                 },
                 {
                     name: 'CI/CD',
                     level: '50%',
+                    icon: 'icon-opensource',
                 },
                 {
                     name: 'Nginx',
                     level: '60%',
+                    icon: 'icon-nginx',
                 },
                 {
                     name: '.NET ',
                     level: '80%',
+                    icon: 'icon-opensource',
                 },
                 {
                     name: 'C#',
                     level: '85%',
+                    icon: 'icon-csharp',
                 },
                 {
                     name: 'Api Rest',
                     level: '90%',
+                    icon: 'icon-opensource',
                 },
             ]
         },
