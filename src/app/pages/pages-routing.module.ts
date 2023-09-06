935e4bc1041c30415dbfe6d04e8ad6eba1075374
Leonel Hacha <leonel.hachasalazar@externos-cl.cencosud.com>
2023-09-06 13:41:43 -0500

    update routes and deployment files

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,32 +2,32 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
-import { LoginComponent } from './login/login.component';
-import { PasswordForgetComponent } from './password-forget/password-forget.component';
-import { SignupComponent } from './signup/signup.component';
+// import { LoginComponent } from './login/login.component';
+// import { PasswordForgetComponent } from './password-forget/password-forget.component';
+// import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
+    // {
+    //     path: '',
+    //     redirectTo: '',
+    //     pathMatch: 'full'
+    // },
     {
         path: '',
-        redirectTo: 'cv',
-        pathMatch: 'full'
-    },
-    {
-        path: 'cv',
         component: MainComponent
     },
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'password_forget',
-        component: PasswordForgetComponent
-    },
-    {
-        path: 'signup',
-        component: SignupComponent
-    },
+    // {
+    //     path: 'login',
+    //     component: LoginComponent
+    // },
+    // {
+    //     path: 'password_forget',
+    //     component: PasswordForgetComponent
+    // },
+    // {
+    //     path: 'signup',
+    //     component: SignupComponent
+    // },
 ];
 
 @NgModule({
